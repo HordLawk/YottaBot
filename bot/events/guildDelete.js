d89ce8b36398616c72b1b0f70c595a713dde9102
hordlawk <pedrofalcaorocha@gmail.com>
2021-06-16 23:30:05 -0300

    ownerid

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -6,7 +6,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor(0xff0000)
             .setAuthor('Left Guild', guild.iconURL({dynamic: true}))
-            .setDescription(`Member count: ${guild.memberCount}\nID: ${guild.id}\nName: ${guild.name}\nOwner: ${guild.owner.user} (${guild.owner.user.tag})\nRegion: ${guild.region}\nFeatures:\`\`\`${guild.features.join('\n')}\`\`\``);
+            .setDescription(`Member count: ${guild.memberCount}\nID: ${guild.id}\nName: ${guild.name}\nOwner: <@${guild.ownerID}>\nRegion: ${guild.region}\nFeatures:\`\`\`${guild.features.join('\n')}\`\`\``);
         guild.client.channels.cache.get(guild.client.configs.guildlog).send(embed);
     },
 };

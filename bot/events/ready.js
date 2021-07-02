f4d845ff85e910f734bcb8fa9c35eccfacaa6a2f
hordlawk <pedrofalcaorocha@gmail.com>
2021-07-02 03:00:54 -0300

    [PATCH] added unpremium timer

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -2,6 +2,7 @@ const channel = require('../../schemas/channel.js');
 const role = require('../../schemas/role.js');
 const menu = require('../../schemas/menu.js');
 const log = require('../../schemas/log.js');
+const guildModel = require('../../schemas/guild.js');
 const {MessageEmbed} = require('discord.js');
 
 module.exports = {
@@ -19,38 +20,48 @@ module.exports = {
         const roleDocs = await role.find({guild: {$in: client.guilds.cache.map(e => e.id)}});
         await role.deleteMany({_id: {$in: roleDocs.filter(e => !client.guilds.cache.get(e.guild).roles.cache.has(e.roleID)).map(e => e._id)}});
         await menu.deleteMany({channelID: {$nin: client.channels.cache.map(e => e.id)}});
+        const unmuteTimer = async () => {
+            const unmutes = await log.find({
+                type: 'mute',
+                duration: {$lte: Date.now()},
+                ongoing: true,
+            });
+            if(!unmutes.length) return;
+            await log.updateMany({_id: {$in: unmutes.map(e => e._id)}}, {$set: {ongoing: false}});
+            for(let unmuteDoc of unmutes){
+                let guild = client.guilds.cache.get(unmuteDoc.guild);
+                if(!guild) continue;
+                let discordMember = await guild.members.fetch(unmuteDoc.target).catch(() => null);
+                let discordChannel = guild.channels.cache.get(client.guildData.get(guild.id).modlogs.mute);
+                if(discordChannel && discordChannel.viewable && discordChannel.permissionsFor(guild.me).has('SEND_MESSAGES') && discordChannel.permissionsFor(guild.me).has('EMBED_LINKS')){
+                    let guildLanguage = client.langs[client.guildData.get(guild.id).language];
+                    let embed = new MessageEmbed()
+                        .setColor(0x0000ff)
+                        .setAuthor(discordMember ? guildLanguage.get('autoUnmuteEmbedAuthorMember', [discordMember.user.tag]) : guildLanguage.get('autoUnmuteEmbedAuthorNoMember'), discordMember?.user.displayAvatarURL({dynamic: true}))
+                        .addField(guildLanguage.get('autoUnmuteEmbedTargetTitle'), guildLanguage.get('autoUnmuteEmbedTargetValue', [unmuteDoc.target]), true)
+                        .setTimestamp()
+                        .addField(guildLanguage.get('autoUnmuteEmbedReasonTitle'), guildLanguage.get('autoUnmuteEmbedReasonValue'));
+                    let msg = await discordChannel.messages.fetch(unmuteDoc.logMessage).catch(() => null);
+                    if(msg) embed.setDescription(guildLanguage.get('autoUnmuteEmbedDescription', [msg.url]));
+                    await discordChannel.send(embed);
+                };
+                if(!guild.me.permissions.has('MANAGE_ROLES') || !client.guildData.get(guild.id).muteRoleID) continue;
+                if(!discordMember) continue;
+                let discordRole = guild.roles.cache.get(client.guildData.get(guild.id).muteRoleID);
+                if(!discordRole || !discordRole.editable || !discordMember.roles.cache.has(discordRole.id)) continue;
+                await discordMember.roles.remove(discordRole);
+            }
+        }
+        const unpremiumTimer = async () => {
+            const endedPremium = await guildModel.find({premiumUntil: {$lt: Date.now()}});
+            if(!endedPremium.length) return;
+            await guildModel.updateMany({premiumUntil: {$lt: Date.now()}}, {$set: {premiumUntil: null}});
+            endedPremium.forEach(e => client.guildData.get(e._id).premiumUntil = null);
+        }
         const tick = () => {
-            setTimeout(async () => {
-                const unmutes = await log.find({
-                    type: 'mute',
-                    duration: {$lte: Date.now()},
-                    ongoing: true,
-                });
-                if(!unmutes.length) return tick();
-                await log.updateMany({_id: {$in: unmutes.map(e => e._id)}}, {$set: {ongoing: false}});
-                for(let unmuteDoc of unmutes){
-                    let guild = client.guilds.cache.get(unmuteDoc.guild);
-                    if(!guild) continue;
-                    let discordMember = await guild.members.fetch(unmuteDoc.target).catch(() => null);
-                    let discordChannel = guild.channels.cache.get(client.guildData.get(guild.id).modlogs.mute);
-                    if(discordChannel && discordChannel.viewable && discordChannel.permissionsFor(guild.me).has('SEND_MESSAGES') && discordChannel.permissionsFor(guild.me).has('EMBED_LINKS')){
-                        let guildLanguage = client.langs[client.guildData.get(guild.id).language];
-                        let embed = new MessageEmbed()
-                            .setColor(0x0000ff)
-                            .setAuthor(discordMember ? guildLanguage.get('autoUnmuteEmbedAuthorMember', [discordMember.user.tag]) : guildLanguage.get('autoUnmuteEmbedAuthorNoMember'), discordMember?.user.displayAvatarURL({dynamic: true}))
-                            .addField(guildLanguage.get('autoUnmuteEmbedTargetTitle'), guildLanguage.get('autoUnmuteEmbedTargetValue', [unmuteDoc.target]), true)
-                            .setTimestamp()
-                            .addField(guildLanguage.get('autoUnmuteEmbedReasonTitle'), guildLanguage.get('autoUnmuteEmbedReasonValue'));
-                        let msg = await discordChannel.messages.fetch(unmuteDoc.logMessage).catch(() => null);
-                        if(msg) embed.setDescription(guildLanguage.get('autoUnmuteEmbedDescription', [msg.url]));
-                        await discordChannel.send(embed);
-                    };
-                    if(!guild.me.permissions.has('MANAGE_ROLES') || !client.guildData.get(guild.id).muteRoleID) continue;
-                    if(!discordMember) continue;
-                    let discordRole = guild.roles.cache.get(client.guildData.get(guild.id).muteRoleID);
-                    if(!discordRole || !discordRole.editable || !discordMember.roles.cache.has(discordRole.id)) continue;
-                    await discordMember.roles.remove(discordRole);
-                }
+            setTimeout(() => {
+                unpremiumTimer();
+                unmuteTimer();
                 tick();
             }, 10000);
         }

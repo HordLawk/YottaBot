5f85aee3d1bf3fbb143c78e7a39aefd187a44631
hordlawk <pedrofalcaorocha@gmail.com>
2021-07-14 22:00:01 -0300

    [PATCH] let not members on ranking

--- bot/commands/xp.js
+++ bot/commands/xp.js
@@ -18,16 +18,11 @@ module.exports = {
         if(!message.client.guildData.get(message.guild.id).gainExp) return message.channel.send(message.client.langs[channelLanguage].get('xpDisabled'));
         switch(args[0]){
             case 'rank': {
-                console.log(process.memoryUsage().heapUsed);
-                let members = await message.guild.members.fetch({cache: false}).then(res => res.map(e => e.id));
-                console.log(process.memoryUsage().heapUsed);
                 let pageSize = 20;
                 let memberDocs = await member.find({
                     guild: message.guild.id,
-                    userID: {$in: members},
                     xp: {$ne: 0},
                 }, 'userID xp').sort({xp: -1}).limit(pageSize);
-                console.log(process.memoryUsage().heapUsed);
                 let embed = new MessageEmbed()
                     .setColor(message.guild.me.displayColor || 0x8000ff)
                     .setAuthor(message.client.langs[channelLanguage].get('xpRankEmbedAuthor'), message.guild.iconURL({dynamic: true}))
@@ -44,7 +39,6 @@ module.exports = {
                     if(r.emoji.name === '➡'){
                         memberDocs = await member.find({
                             guild: message.guild.id,
-                            userID: {$in: members},
                             xp: {$ne: 0},
                         }, 'userID xp').sort({xp: -1}).skip((page + 1) * pageSize).limit(pageSize);
                         if(!memberDocs.length) return;
@@ -54,12 +48,10 @@ module.exports = {
                         if(!page) return;
                         memberDocs = await member.find({
                             guild: message.guild.id,
-                            userID: {$in: members},
                             xp: {$ne: 0},
                         }, 'userID xp').sort({xp: -1}).skip((page - 1) * pageSize).limit(pageSize);
                         page--;
                     }
-                    console.log(process.memoryUsage().heapUsed);
                     embed.setDescription(memberDocs.map((e, i) => `${(e.userID === message.author.id) ? '__' : ''}**#${page * pageSize + (i + 1)} -** <@${e.userID}> **|** \`${e.xp}xp\`${(e.userID === message.author.id) ? '__' : ''}`).join('\n'));
                     await msg.edit(embed);
                 });

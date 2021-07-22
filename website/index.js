c7009eedc28f6c66eaceec102999b209f79f03df
hordlawk <pedrofalcaorocha@gmail.com>
2021-07-22 07:02:39 -0300

    [WEB] topgg endpoint

--- website/index.js
+++ website/index.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/docs', (req, res) => res.redirect('https://github.com/HordLawk/YottaBot#get-started'));
 
+app.get('/topgg', (req, res) => res.redirect('https://top.gg/bot/371902120561082368'));
+
 app.get('*', (req, res) => res.redirect(`https://discord.com/api/oauth2/authorize?client_id=371902120561082368&permissions=${Discord.Permissions.ALL}&scope=bot`));
 
 app.listen(process.env.PORT || port, () => console.log(`Server listening at port ${process.env.PORT || port}`));

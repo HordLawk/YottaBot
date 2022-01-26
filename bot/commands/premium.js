05945da1fb429f840e1da1938b0e90f6ca2a5a56
HordLawk <pedrofalcaorocha@gmail.com>
2022-01-26 02:19:59 -0300

    [PATCH] support different names for context menus

--- bot/commands/premium.js
+++ bot/commands/premium.js
@@ -44,7 +44,6 @@ module.exports = {
                 }],
             });
             const collector = msg.createMessageComponentCollector({
-                filter: componentInteraction => (componentInteraction.user.id === message.author.id),
                 idle: 10000,
                 max: 1,
                 componentType: 'BUTTON',

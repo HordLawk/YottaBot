5a8f7b654d908b1b4dde64940926cfdf4ab9176a
HordLawk <pedrofalcaorocha@gmail.com>
2022-02-15 16:08:46 -0300

    [PATCH] testing on productionnnn

--- website/api.js
+++ website/api.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/pledges', async (req, res) => {
-    console.log(JSON.stringify(req, null, 4));
+    switch(req.header('X-Patreon-Event')){
+        case 'members:pledge:create': {
+            console.log('created pledge');
+        }
+        break;
+        case 'members:pledge:delete': {
+            console.log('deleted pledge')
+        }
+        break;
+    }
+    console.log(req);
 });
 
 module.exports = router;

ae25cc8eb967937f94ad159e89542feef7962815
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-05-16 12:45:32 -0300

    feat(eval): added a modal to write eval code

--- bot/commands/eval.js
+++ bot/commands/eval.js
@@ -1,8 +1,36 @@
 module.exports = {
     active: true,
     name: 'eval',
+    description: () => 'Directly evaluates JavaScript code',
     dev: true,
     args: true,
     usage: () => ['(code)'],
     execute: async message => eval(message.content.replace(/^\S+\s+/, '')),
+    executeSlash: async interaction => {
+        interaction.awaitModalSubmit({
+            filter: i => (i.customId === 'eval') && (i.user.id === interaction.client.application.owner.id),
+            time: 600_000,
+        }).then(async i => {
+            await i.reply({
+                content: 'Executing code...',
+                ephemeral: true,
+            });
+            eval(i.fields.getTextInputValue('code'));
+        });
+        await interaction.showModal({
+            customId: 'eval',
+            title: 'eval',
+            components: [{
+                type: 'ACTION_ROW',
+                components: [{
+                    type: 'TEXT_INPUT',
+                    customId: 'code',
+                    label: 'Code',
+                    placeholder: 'console.log("Hello, World!");',
+                    required: true,
+                    style: 'PARAGRAPH',
+                }],
+            }],
+        });
+    },
 };

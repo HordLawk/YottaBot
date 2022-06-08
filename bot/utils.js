94cb5ce65a25bc623d19e10f651a24a1d1ae25c8
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-06-07 23:33:05 -0300

    fix(utils): locale path

--- bot/utils.js
+++ bot/utils.js
@@ -1,4 +1,4 @@
-const locale = require('../../locale');
+const locale = require('../locale');
 
 const getStringLocales = key => locale.reduce((acc, e) => e.get(key) ? {...acc, [e.code]: e.get(key)} : acc, {});
 

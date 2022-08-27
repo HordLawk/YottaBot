feb86458ece8167bca8530b1f71cd11fc095d743
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-08-27 09:08:37 -0300

    fix(guildlog): work on guilds without feature

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -11,7 +11,7 @@ module.exports = {
                 name: 'Left Guild',
                 iconURL: guild.iconURL({dynamic: true}),
             })
-            .setDescription(`Member count: ${guild.memberCount}\nID: ${guild.id}\nName: ${guild.name}\nOwner: <@${guild.ownerId}>\nLocale: ${guild.preferredLocale}\nFeatures:\`\`\`${guild.features.join('\n')}\`\`\``);
+            .setDescription(`Member count: ${guild.memberCount}\nID: ${guild.id}\nName: ${guild.name}\nOwner: <@${guild.ownerId}>\nLocale: ${guild.preferredLocale}\nFeatures:\`\`\`${guild.features?.join('\n')}\`\`\``);
         await guild.client.channels.cache.get(configs.guildlog).send({embeds: [embed]});
         guild.client.channels.cache.get(configs.guildlog).setTopic(guild.client.guilds.cache.size);
     },

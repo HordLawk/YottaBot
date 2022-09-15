a9c3c459dc5d21606e45a88515f25744e4c8fdca
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-09-15 20:20:17 -0300

    fix(error): method to check command interaction

--- bot/events/interactionCreate.js
+++ bot/events/interactionCreate.js
@@ -4,7 +4,7 @@ const interactions = require('../interactions');
 module.exports = {
     name: 'interactionCreate',
     execute: async interaction => interactions.get(interaction.type)?.execute(interaction).catch(error => {
-        if(interaction.isApplicationCommand()) interaction.reply({
+        if(interaction.isCommand()) interaction.reply({
             content: locale.get((interaction.locale === 'pt-BR') ? 'pt' : 'en').get('error', [interaction.commandName]),
             ephemeral: true,
         });

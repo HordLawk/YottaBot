503fe8e08670eb583103a58eabe99bbec0079a0e
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-09-15 13:33:35 -0300

    fix(joinlog): updated flag names

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -21,23 +21,44 @@ module.exports = {
                     })
                     .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
                     .setDescription(member.toString());
-                if((member.user.flags?.toArray().length || member.user.bot) && member.guild.roles.everyone.permissionsIn(hook.channelId).has(PermissionsBitField.Flags.UseExternalEmojis)){
+                if(
+                    (
+                        member.user.flags?.remove(
+                            UserFlags.Quarantined
+                            +
+                            UserFlags.TeamPseudoUser
+                            +
+                            UserFlags.Spammer
+                        ).bitfield
+                        ||
+                        member.user.bot
+                    )
+                    &&
+                    member.guild.roles.everyone
+                        .permissionsIn(hook.channelId)
+                        .has(PermissionsBitField.Flags.UseExternalEmojis)
+                ){
                     const badges = {
-                        DISCORD_EMPLOYEE: '<:staff:967043602012315658>',
-                        PARTNERED_SERVER_OWNER: '<:partner:967043547561852978>',
-                        HYPESQUAD_EVENTS: '<:hs:967048946612572160>',
-                        BUGHUNTER_LEVEL_1: '<:bughunter:967043119407329311>',
-                        HOUSE_BRAVERY: '<:bravery:967043119780610058>',
-                        HOUSE_BRILLIANCE: '<:brilliance:967043119780597860>',
-                        HOUSE_BALANCE: '<:balance:967043119809974272>',
-                        EARLY_SUPPORTER: '<:earlysupporter:967043119717699665>',
-                        BUGHUNTER_LEVEL_2: '<:bughunter2:967043119759642694>',
-                        EARLY_VERIFIED_BOT_DEVELOPER: '<:botdev:967043120984391752>',
-                        DISCORD_CERTIFIED_MODERATOR: '<:mod:967043119788994610>',
-                        VERIFIED_BOT: '<:verifiedbot:967049829568090143>',
+                        Staff: '<:staff:967043602012315658>',
+                        Partner: '<:partner:967043547561852978>',
+                        Hypesquad: '<:hs:967048946612572160>',
+                        BugHunterLevel1: '<:bughunter:967043119407329311>',
+                        HypeSquadOnlineHouse1: '<:bravery:967043119780610058>',
+                        HypeSquadOnlineHouse2: '<:brilliance:967043119780597860>',
+                        HypeSquadOnlineHouse3: '<:balance:967043119809974272>',
+                        PremiumEarlySupporter: '<:earlysupporter:967043119717699665>',
+                        BugHunterLevel2: '<:bughunter2:967043119759642694>',
+                        VerifiedDeveloper: '<:botdev:967043120984391752>',
+                        CertifiedModerator: '<:mod:967043119788994610>',
+                        VerifiedBot: '<:verifiedbot:967049829568090143>',
+                        BotHTTPInteractions: '<:bot:967062591190995004>',
                     };
                     const userBadges = member.user.flags.toArray().map(e => badges[e]);
-                    if(!member.user.flags.has(UserFlags.VerifiedBot) && member.user.bot) userBadges.push('<:bot:967062591190995004>');
+                    if(
+                        !member.user.flags.any(UserFlags.VerifiedBot + UserFlags.BotHTTPInteractions)
+                        &&
+                        member.user.bot
+                    ) userBadges.push('<:bot:967062591190995004>');
                     embed.addField(channelLanguage.get('memberjoinEmbedBadgesTitle'), userBadges.join(' '));
                 }
                 embed.addField(channelLanguage.get('memberjoinEmbedCreationTitle'), channelLanguage.get('memberjoinEmbedCreationValue', [Math.round(member.user.createdTimestamp / 1000)]));

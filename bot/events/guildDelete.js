b00c03d8ec29ddca463af6a2373b1b7e58b660db
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-11-16 11:20:30 -0300

    docs(version): matched the merged changes

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-const {EmbedBuilder} = require('discord.js');
 const configs = require('../configs.js');
 
 module.exports = {
